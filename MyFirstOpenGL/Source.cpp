24e7c94e455f79223e48beeef9d8847bb913c145
Adrian Povill <adrian.povill@estudiant.enti.cat>
2026-03-16 18:02:37 +0100

    Dibuixem un punt

--- MyFirstOpenGL/Source.cpp
+++ MyFirstOpenGL/Source.cpp
@@ -1,8 +1,15 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm.hpp>
+#include <iostream>
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
+
+void ResizeWindow(GLFWwindow* window, int iNewFrameBufferWidth, int iNewFrameBufferHeight) {
+
+    // Es defineix la mida del viewport
+    glViewport(0, 0, iNewFrameBufferWidth, iNewFrameBufferHeight);
+}
 
 void main()
 {
@@ -18,7 +25,7 @@ void main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
     // La finrestra és reescalable? 
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Richard", NULL, NULL);
     // Si volem fer pantalla completa podem posar al primer NULL glfwGetPrimaryMonitor()
@@ -28,25 +35,85 @@ void main()
         glfwTerminate();
 
     // Em retorna el tamany del buffer que puc pintar, normalment es la mida del viewport(la finestra generada)
-    glfwGetFramebufferSize(window, &iFrameBufferWidth, &iFrameBufferHeight);
+    glfwSetFramebufferSizeCallback(window, ResizeWindow);   // se li passa la finestra que es reescala i 
+                                                            // la funció que s'executarà quan es reescali
 
-
-
-    /* Make the window's context current */
+    // Defineixo la finestra sobre la que treballarem
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+    // Per poder fer servir funcions que per a qui no tingui actualitzats els drivers podrien considerarse
+    // experimentals tot i que actualment no ho siguin, per asegurarnos que funcionen en qualsevol tarj.gràf.
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+    glewExperimental = GL_TRUE;
 
-        /* Poll for and process events */
-        glfwPollEvents();
+    if (glewInit() == GLEW_OK) {
+        // Ha furulao
+
+        // Li definim el color base del buffer
+        glClearColor(1.f, 0.f, 0.f, 1.f/*vermell*/);
+
+        GLuint vaoPuntos, vboPuntos;
+
+        // Genero 1 VAO i l'emmagatzemo a vaoPuntos
+        glGenVertexArrays(1, &vaoPuntos); //Li dic la quantitat de VAOs i li dono la variable on vull que els guardi
+
+        // Activo vaopuntos per a treballar
+        glBindVertexArray(vaoPuntos);
+
+        // Genero 1 VBO i l'emmagatzemo a vboPuntos
+        glGenBuffers(1, &vboPuntos);
+
+        // Indico quin VBO es l'actiu i que aquest emmagatzema un array de dades
+        glBindBuffer(GL_ARRAY_BUFFER, vboPuntos);
+
+        // Declaro puntos {x, y}
+        GLfloat punto[] = { 0.f, 0.f };
+
+        glBufferData(GL_ARRAY_BUFFER, sizeof(punto), punto, GL_STATIC_DRAW);
+
+        // indico com llegir la informació
+        glVertexAttribPointer(0, 2/*de 0 a 2 (2D)*/, 
+            GL_FLOAT/*Tipus de dada*/, 
+            GL_FALSE /*Si tenim un model escalat, això ho normalitza si esta en true*/,
+            2* sizeof(GLfloat) /*Quantes vegades s'ha de desplaçar el punter, 
+            com son dos floats s'ha de moure els bits que ocupen dos floats*/, 
+            (GLvoid*) 0/*No massa important, millor a 0 sempre*/);
+
+        // Active, les ddades per a que la gpu pugui usar-les
+        glEnableVertexAttribArray(0);
+
+        //Desvinculo VBO
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        //Desvinculo VAO
+        glBindVertexArray(0);
+
+        while (!glfwWindowShouldClose(window)) {
+            // Detecció de inputs
+            glfwPollEvents();
+
+            // Netejem tots els buffers
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+            // Activo VAO amb la geometria carregada
+            glBindVertexArray(vaoPuntos);
+
+            // Dibuixo la geometria
+            glDrawArrays(GL_POINTS, 0, 1);
+
+            // Desactivo VAO amb la geometria carregada
+            glBindVertexArray(0);
+
+            // Canviem els Buffers
+            glFlush(); // Flush procura la sincronització de Frame Rate i Refresh Rate
+            glfwSwapBuffers(window);
+        }
     }
-
+    else
+    {
+        // No funca
+        // Tanquem connexió amb la targeta
+        glfwTerminate();
+    }
     glfwTerminate();
 }
